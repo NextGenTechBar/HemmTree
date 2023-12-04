e4039b6dd2415abd00e4ceed5a449d41014c5f22
BlaineAtkins <blainegatkins@gmail.com>
2023-12-03 22:24:10 -0800

    Add files via upload

--- Stats Programs/StatsReader.py
+++ Stats Programs/StatsReader.py
@@ -1,9 +1,83 @@
-#This program reads in a stats file and counts only the non-repeating values. For example, (red,green,green,green,red) would count as 3 instead of 5
-#in the future, it would be good to output a second stats csv that only contains the non-repeated values
-
-#it also prints a the number of unique users, and a csv-formatted list with a row for each user, and a column for total commands and "useful" commands (non-repeating ones)
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import csv
+import time
+
+#####functions
+def cell(text: str, num: int):
+    if(num==1):
+        return(text[0:find_nth(text,",",num)])
+    else:
+        if(find_nth(text,",",num)==-1): #last column
+            return(text[find_nth(text,",",num-1)+1:])
+        else:
+            return(text[find_nth(text,",",num-1)+1:find_nth(text,",",num)])
+
+def find_nth(haystack: str, needle: str, n: int) -> int:
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+#####
+
+####this part is just an inefficient way to filter out duplicate commands
+def countCommands():
+    uniqueCommands=0
+    lastCommand=""
+    with open('HemmTreeStats.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            rowValue=(', '.join(row))
+            command=cell(rowValue,3)
+            if(command!=lastCommand):
+                uniqueCommands+=1
+            lastCommand=command
+    return(uniqueCommands)
+####
+
+
+fig = plt.figure()
+while(True):
+    data = pd.read_csv('HemmTreeStats.csv')
+
+
+    user_counts = data.iloc[:,3].value_counts()
+
+    uniqueUsers=len(user_counts)
+    numCommands=len(data)
+    uniqueCommands=str(countCommands())
+
+    ax=fig.add_subplot(111)
+
+    bin_edges = [0,10.1,20.1,30.1,40.1,50.1,60]
+    ax.hist(np.clip(user_counts,bin_edges[0],bin_edges[-1]), #clip makes all values above the max bin edge to fall within the last bin as an overflow
+    bins=bin_edges,edgecolor='black')
+    plt.xlabel('Number of Commands Sent')
+    plt.ylabel('Number of Users')
+    plt.title('Number of commands sent per user')
+    plt.xticks(range(0,51,5))
+
+    ax.text(0.5,1.15,"Christmas Tree Stats",fontsize=14,ha='center',va='top',weight='bold',color='green',transform=ax.transAxes)
+    ax.text(55,-7,">50",fontsize=10,ha='center')
+    userNumText=str(uniqueUsers)+" Unique Users"
+    ax.text(.7,.87,userNumText,fontsize=12,ha='center',transform=ax.transAxes)
+    commandsText=str(uniqueCommands)+" Commands Sent"
+    ax.text(.7,.8,commandsText,fontsize=12,ha='center',transform=ax.transAxes)
+
+
+    plt.draw()
+    #time.sleep(10) #regenerate plot every x mins
+    plt.pause(60)
+    fig.clear()
+    print("Updating chart...")
+
+
+
+
+
+
 
 def cell(text: str, num: int):
     if(num==1):
@@ -20,53 +94,3 @@ def find_nth(haystack: str, needle: str, n: int) -> int:
         start = haystack.find(needle, start+len(needle))
         n -= 1
     return start
-
-lastCommand=""
-commandCtr=0;
-totalCommandCtr=0
-
-with open('HemmTreeStats.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        rowValue=(', '.join(row))
-        command=cell(rowValue,3)
-        if(command!=lastCommand):
-            commandCtr+=1
-        totalCommandCtr+=1
-        lastCommand=command
-
-print("Total Raw Commands: " +str(totalCommandCtr))
-print("Non-duplicate commands: "+str(commandCtr))
-
-
-userIDs=[]
-#populate list with unique users
-with open('HemmTreeStats.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        rowValue=(', '.join(row))
-        user=cell(rowValue,4)
-        if(not(user in userIDs)):
-            userIDs.append(user)
-
-print("Unique Users: "+str(len(userIDs)))
-
-commandsPerUser = [0]*len(userIDs)
-commandsPerUserRemovedDuplicates = [0]*len(userIDs)
-
-for i in range(len(userIDs)):
-    with open('HemmTreeStats.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            rowValue=(', '.join(row))
-            user=cell(rowValue,4)
-            command=cell(rowValue,3)
-            if(user==userIDs[i]):
-                commandsPerUser[i]+=1
-                if(command!=lastCommand):
-                    commandsPerUserRemovedDuplicates[i]+=1
-            lastCommand=command
-
-print("UserID,total commands,useful commands")
-for i in range(len(userIDs)):
-    print(str(userIDs[i])+","+str(commandsPerUser[i])+","+str(commandsPerUserRemovedDuplicates[i]))
